4f25b5f7aab386cc33bd04f525210fcbb8747dbb
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-06-14 18:07:30 +0200

    validation to models

--- app/models/balcony.rb
+++ app/models/balcony.rb
@@ -4,4 +4,12 @@ class Balcony < ActiveRecord::Base
   has_and_belongs_to_many :items
   belongs_to :user
 
+  validates :type, inclusion: { in: %w(small medium large)}
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :department, presence: true
+  validates :length, presence: true
+  validates :orientation, presence: true
+  validates :user_id, presence: true
+
 end
