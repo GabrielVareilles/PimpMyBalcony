6d1101acf534cd3002b5ccd474a11be35f4fa6b9
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-06-14 15:21:42 +0200

    controller balconies started

--- app/controllers/balconies_controller.rb
+++ app/controllers/balconies_controller.rb
@@ -1,5 +1,10 @@
 class BalconiesController < ApplicationController
+
+  skip_before_action :authenticate_user!
+  before_action :set_balcony, only: [:show, :edit, :update, :destroy]
+
   def index
+    @balconies = Balcony.all
   end
 
   def show_public
@@ -9,17 +14,46 @@ class BalconiesController < ApplicationController
   end
 
   def new
+    @balcony = Balcony.new
   end
 
   def create
+    @balcony = current_user.balconies.build(balcony_params)
+    @balcony.user_id = current_user.id if current_user
+    if @balcony.save
+      redirect_to private_balcony_path(@balcony)
+      flash[:notice] = "Balcony successfuly created"
+    else
+      flash[:notice] = "Balcony not created"
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @balcony.update(balcony_params)
+      redirect_to private_balcony_path(@balcony)
+      flash[:notice] = "Food truck successfuly edited"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @balcony.destroy
+    redirect_to balconys_path
   end
+
+  private
+
+  def balcony_params
+    params.require(:balcony).permit(:name)
+  end
+
+  def set_balcony
+    @balcony = Balcony.find(params[:id])
+  end
+
 end
