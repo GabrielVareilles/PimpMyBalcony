e9b35ce70318d9f56aea609597795c9cd368e970
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-06-14 16:01:46 +0200

    item policy added

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,11 @@ class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @items = Item.all
+    @items = policy_scope(Item)
   end
 
   def show
     @item = Item.find(params[:id])
+    autorize @item
   end
 end
---
+++ app/policies/item_policy.rb
@@ -0,0 +1,12 @@
+class ItemPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show
+    true
+  end
+
+end
