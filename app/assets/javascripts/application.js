8babb5e3597e112020ce0b6ea4892f8d821de0f2
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-15 19:10:58 +0200

    neomax js imported

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,33 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 
+// Neomax JS
+//= require appear.js
+//= require contact.js
+//= require custom.js
+//= require custom.min.js
+//= require gmap3.js
+//= require imagesloaded.pkgd.js
+//= require isotope.pkgd.js
+//= require jarallax-video.js
+//= require jarallax.js
+//= require jqBootstrapValidation.js
+//= require jquery-2.2.3.js
+//= require jquery-2.2.3.min.js
+//= require jquery.countTo.js
+//= require jquery.fitvids.js
+//= require jquery.flexslider.js
+//= require jquery.magnific-popup.js
+//= require jquery.singlePageNav.js
+//= require jquery.sticky-kit.js
+//= require jquery.zoom.js
+//= require owl.carousel.js
+//= require plugins.js
+//= require plugins.min.js
+//= require smoothscroll.js
+//= require submenu-fix.js
+//= require twitterFetcher.js
+//= require wow.js
 
-
+// at the end
 //= require_tree .
