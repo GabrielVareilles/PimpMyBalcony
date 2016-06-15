3ea633d9a47ee6cac45b23a7319c46caadb79f2b
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-15 11:14:26 +0200

    active admine set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  ActiveAdmin.routes(self)
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   scope '(:locale)', locale: /fr|en/ do
     devise_for :users, skip: :omniauth_callbacks
