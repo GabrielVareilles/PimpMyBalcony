53f93d6dc363e42262b39aa5f62b3125d587cea4
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-16 14:43:11 +0200

    font awesomes solution by Yahn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,16 @@
 //= require bootstrap-sprockets
 
 // Neomax JS
+//= require jquery-2.2.3.js
+//= require jquery.countTo.js
+//= require jquery.fitvids.js
+//= require jquery.flexslider.js
+//= require jquery.magnific-popup.js
+//= require jquery.singlePageNav.js
+//= require jquery.sticky-kit.js
+//= require jquery.zoom.js
+//= require bootstrap
+//= require bootstrap-sprockets
 //= require appear.js
 //= require contact.js
 //= require custom.js
@@ -13,15 +23,6 @@
 //= require jarallax-video.js
 //= require jarallax.js
 //= require jqBootstrapValidation.js
-//= require jquery-2.2.3.js
-//= require jquery-2.2.3.min.js
-//= require jquery.countTo.js
-//= require jquery.fitvids.js
-//= require jquery.flexslider.js
-//= require jquery.magnific-popup.js
-//= require jquery.singlePageNav.js
-//= require jquery.sticky-kit.js
-//= require jquery.zoom.js
 //= require owl.carousel.js
 //= require plugins.js
 //= require plugins.min.js
@@ -29,6 +30,7 @@
 //= require submenu-fix.js
 //= require twitterFetcher.js
 //= require wow.js
+//= require cloudinary
 
 // at the end
 //= require_tree .
