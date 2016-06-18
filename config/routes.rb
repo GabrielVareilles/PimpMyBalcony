01a94ca5d2858fce1c947dcd31b9b6e97df1653f
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-18 17:23:00 +0200

    add order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'orders/new'
+
+  get 'orders/create'
+
+  get 'orders/show'
+
+  get 'orders/edit'
+
+  get 'orders/update'
+
+  get 'orders/destroy'
+
   ActiveAdmin.routes(self)
   devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   scope '(:locale)', locale: /fr|en/ do
@@ -8,6 +20,7 @@ Rails.application.routes.draw do
     get "inspiration", to: "pages#inspiration", as: 'inspiration'
     resources :users, only: [:edit, :update]
     get "profile", to: "users#show", as: 'profile'
+    resources :orders, only: [:edit, :update]
 
     resources :balconies, except: [:show] do
       resources :reviews, only: :create
@@ -20,7 +33,7 @@ Rails.application.routes.draw do
     get "balconies_private/:id", to: "balconies#show_private", as: 'private_balcony'
     get "balconies_public/:id", to: "balconies#show_public", as: 'public_balcony'
     get "complete", to: "balconies#complete", as: 'complete'
-    
+
     resources :items, only: [:index, :show, :destroy] do
       member do
         post "add_plant"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160618145459) do
+ActiveRecord::Schema.define(version: 20160618152009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 20160618145459) do
     t.string   "delivery_adress"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "status"
   end
 
   create_table "plants", force: :cascade do |t|
