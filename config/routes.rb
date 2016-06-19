5b71d1c09dbffdf09c96501074a4377e20c2c8e4
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-06-19 16:20:48 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
     resources :users, only: [:edit, :update]
     get "profile", to: "users#show", as: 'profile'
 
-    resources :orders, only: [:show, :new] do
+    get "cart", to: "orders#show", as: 'cart'
+    resources :orders, only: [:new] do
       member do
         post "add_item"
         post "remove_item"
