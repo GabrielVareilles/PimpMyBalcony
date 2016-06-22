e66f265c61e28e6e3e2bffa6e2514b882b45de51
gabriel vareilles <gabriel.vareilles@gmail.com>
2016-06-22 17:19:16 +0200

    fix

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -7,9 +7,9 @@ $(".dynamic-remove-item-cart").on("ajax:success", function (e, data, status, xhr
   $("div[data-id='" + data.id + "']").remove();
 }).on("ajax:error", function (e, xhr, status, error) {
 });
-
+// ADDTOCART
 $(".dynamic-add-to-cart-item").on("ajax:success", function (e, data, status, xhr) {
-  $("div[data-id='additem" + data.id + "']").append("<i class="fa fa-check-circle" aria-hidden="true"></i><h5> added to your cart</h5>"
+  $("div[data-id='additem" + data.id + "']").append('<h2> <i class="fa fa-cart-arrow-down fa-lg" style="color:green"></i></h2>'
 );
 }).on("ajax:error", function (e, xhr, status, error) {
 });
